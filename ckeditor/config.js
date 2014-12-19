27423a7f5f7e98811ed1ba8810b6b41754bb0f81
BAILLOT Mickael <mickael.baillot@gmail.com>
2014-12-19 15:49:53 +0100

    integration kcfinder

--- ckeditor/config.js
+++ ckeditor/config.js
@@ -7,4 +7,10 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+		config.filebrowserBrowseUrl = '../kcfinder/browse.php?opener=ckeditor&type=files';
+	   config.filebrowserImageBrowseUrl = '../kcfinder/browse.php?opener=ckeditor&type=images';
+	   config.filebrowserFlashBrowseUrl = '../kcfinder/browse.php?opener=ckeditor&type=flash';
+	   config.filebrowserUploadUrl = '../kcfinder/upload.php?opener=ckeditor&type=files';
+	   config.filebrowserImageUploadUrl = '../kcfinder/upload.php?opener=ckeditor&type=images';
+	   config.filebrowserFlashUploadUrl = '../kcfinder/upload.php?opener=ckeditor&type=flash';
 };
--- config/db.php
+++ config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=webchercheur',
     'username' => 'root',
-    'password' => '3010potter',
+    'password' => '',
     'charset' => 'utf8',
 ];
